19da1d228d608e85595a744147e7cd6da5cc24fe
Frezer <frezerzewdu@gmail.com>
2023-07-07 21:03:20 +0400

    tailwind added to project

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tw`flex flex-col justify-center items-center w-full`}>
+      <View style={tw`flex flex-col justify-center items-center w-full`}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <view style={tw`bg-blue-300 h-11 w-full`}></view>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
